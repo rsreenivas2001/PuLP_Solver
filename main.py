2259cf0cd00f9131c6773058c1fec065c7a5e747
Sreenivas <rsreenivas2001@gmail.com>
2021-04-01 00:58:57 +0530

    Added assignment prblm

--- main.py
+++ main.py
@@ -4,87 +4,7 @@ import numpy as np
 from utils.transport_utils import *
 from utils.assign_utils import *
 
-'''def find_cost(r, c):
-    cost_lst = []
-    for i in range(0, row-1):
-        for j in range(0, col-1):
-            cost_lst.append(df[j][i])
-    # print(cost_lst)
-    cost = np.array(cost_lst)
 
-# pulp.pulpTestAll()
-
-df = pd.read_csv('data.csv', header=None)
-
-row, col = df.shape
-
-print(row, col)
-
-row_ = row - 1
-col_ = col - 1
-
-cost_lst = []
-
-for i in range(0, row-1):
-    for j in range(0, col-1):
-        cost_lst.append(df[j][i])
-
-# print(cost_lst)
-
-cost = np.array(cost_lst)
-
-print(cost)
-
-
-var_count = (row_)*(col_)
-cons_count = row_ + col_
-
-# print(type(row))
-# print(type(col))
-
-coeff_lst = []
-
-count = int(var_count)
-for i in range(0, cons_count):
-    r = [0] * var_count
-    # print(r)
-    coeff_lst.append(r)
-
-# print(coeff_lst)
-
-
-buff = 0
-for i in range(0, row_):
-    for j in range(0, col_):
-        coeff_lst[i][j + buff] = 1
-    buff += col_
-
-buff = 0
-for i in range(row_, cons_count):
-    for j in range(0, var_count, col_):
-        coeff_lst[i][j + buff] = 1
-    buff += 1
-
-# print(coeff_lst)
-
-coeff = np.array(coeff_lst)
-
-print(coeff)
-
-resource_lst = []
-
-for i in range(0, row_):
-    resource_lst.append(df[col_][i])
-
-for j in range(0, col_):
-    resource_lst.append(df[j][row_])
-
-# print(resource_lst)
-
-resource = np.array(resource_lst)
-
-print(resource)
-'''
 def transportation():
     data_frame = pd.read_csv('data.csv', header=None)
 
@@ -107,11 +27,12 @@ def assignment():
 
     print(row, col)
 
-    a = assign_find_cost(data_frame,row, col)
-    b = assign_find_coeff(row, col)
-    c = assign_find_resource(data_frame,row, col)
+    cost = assign_find_cost(data_frame,row, col)
+    coeff = assign_find_coeff(row, col)
+    resource = assign_find_resource(data_frame,row, col)
 
-    print(a,b,c,sep="\n")
+    #print(cost,coeff,resource,sep="\n")
+    assign_solver(cost, coeff, resource, row, col)
 
 
 def main():
--- utils/assign_utils.py
+++ utils/assign_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
+from pulp import *
 
-def assign_find_cost(df,r, c):
+def assign_find_cost(df, r, c):
     cost_lst = []
     row = r
     col = c
@@ -15,7 +16,7 @@ def assign_find_cost(df,r, c):
     return cost
 
 
-def assign_find_coeff(r,c):
+def assign_find_coeff(r, c):
     coeff_lst = []
 
     row = r
@@ -50,15 +51,48 @@ def assign_find_coeff(r,c):
     return coeff
 
 
-def assign_find_resource(df,r,c):
-    resource_lst = []
+def assign_find_resource(df, r, c):
     
     var_count = r*c
     cons_count = r + c
-    resource_lst.append([1] * cons_count)
+    resource_lst = ([1] * cons_count)
 
     # print(resource_lst)
 
     resource = np.array(resource_lst)
 
     return resource
+
+
+def assign_solver(cost, coeff, resources, r, c):
+    # Define the model
+    model = LpProblem("LPP", LpMinimize)
+
+    var_count = r * c
+    cons_count = r + c
+    print(var_count, cons_count)
+    # Define the decision variable
+    x = {i: LpVariable(name=f"x{i}", lowBound=0, cat='Integer') for i in range(1, var_count+1)}
+
+    # Define The objective
+    model += lpSum(cost[i-1]*x[i] for i in range(1, var_count+1))
+
+    # Add Constraints
+    for j in range(cons_count):
+        model += lpSum( coeff[j, i-1] * x[i] for i in range(1, var_count+1)) == resources[j]
+
+    # Print model
+    print(model)
+
+    # Solve the optimization problem
+    status = model.solve(PULP_CBC_CMD(msg=False))
+
+    # Get the results
+    print('##############Solution of LPP is #################n\n')
+    print(f"status: {model.status}, {LpStatus[model.status]}")
+    print(f"objective: {model.objective.value()}")
+
+    for var in x.values():
+        print(f"{var.name}: {var.value()}")
+
+
